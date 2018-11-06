7a41d92f0893cc0d10d592b0effd6044749fb712
Olivier ESUKA <oesukam@gmail.com>
2018-11-06 15:52:38 +0200

    Morgan logger configured

--- src/server.js
+++ src/server.js
@@ -2,10 +2,19 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import helmet from 'helmet';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
 
 dotenv.config();
 const app = express();
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, NODE_ENV } = process.env;
+
+// Check for working environment to start logging http request
+if (NODE_ENV === 'development') {
+  app.use(morgan('tiny'));
+  console.log('Morgan enabled');
+}
+
+app.use(helmet()); // Sets various http headers
 
 /* Apply the body-parser middleware to grab data
   from the request body and create application/json parser
@@ -13,7 +22,6 @@ const { PORT = 3000 } = process.env;
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use(helmet); // Sets various http headers
 
 app.get('/', (req, res) => {
   res.json({ msg: 'hello world' });
