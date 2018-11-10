4899a285e32f1bafeabc9e5086514de42d135b3a
Olivier ESUKA <oesukam@gmail.com>
2018-11-10 23:33:40 +0200

    sendConfirmEmail function immproved

--- src/controllers/mail.js
+++ src/controllers/mail.js
@@ -10,30 +10,23 @@ const sendConfirmEmail = (user = '') => {
         </div>
         <p style="font-size: 1.2rem; line-height: 2rem; color: #5a5a5a;">
           Hello ${user.firstName}, <br>
-          Thank you for creating an account with us, please proceed to
-          to confirm your email.
+          Thank you for creating an account with us, please proceed to confirm your email.
         <p/>
         <div style="text-align: center; padding: 20px;">
-          <a 
-            href="${URL}/api/v1/users/${user.id}/confirmEmail/${user.confirmationCode}"
+          <a href="${URL}/api/v1/users/${user.id}/confirmEmail/${user.confirmationCode}"
             style="color: #fff; background-color: #3359DF; padding: 10px 20px; font-size: 1.2rem; text-align: center; text-decoration: none;"
-          >
-            Confirm email
-          </a>
+          > Confirm email </a>
           <p style="font-size: 1.5rem; margin-top: 30px; color: #5a5a5a !important">
             Or copy the link below
-          <p>
-          <br>
+          <p><br>
           ${URL}/api/v1/users/${user.id}/confirmEmail/${user.confirmationCode} 
         </div>
-
-        <p style="color: #5a5a5a !important;"
+        <p style="color: #5a5a5a !important;>
           Thank you, <br>
           Andela - SendIT Team
         </p>
       </div>
     `;
-
     mailer({ subject: 'Email confirmation', to: user.email, html: mailBody });
   }
 };
