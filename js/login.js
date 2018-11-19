db9082ef13b4a58fae82417a07f5daf5c59b878a
Olivier ESUKA <oesukam@gmail.com>
2018-11-19 21:29:56 +0200

    [Bug #162064052] Move web pages from UI directory to root

--- js/login.js
+++ js/login.js
@@ -3,42 +3,44 @@
 (() => {
   let form = {
     email: '',
-    password: ''
-  }
-  const testEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    password: '',
+  };
+  const testEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
   const email = document.querySelector('#email');
+  const emailError = document.querySelector('.form-error.email');
   const password = document.querySelector('#password');
+  const passwordError = document.querySelector('.form-error.password');
   const submitLogin = document.querySelector('#submit-login');
 
   email.addEventListener('input', inputHandler);
   password.addEventListener('input', inputHandler);
   submitLogin.addEventListener('click', (e) => {
     e.preventDefault();
-    
+
     // Reset form errors to empty string
-    document.querySelector('.form-error.email').textContent = '';
-    document.querySelector('.form-error.password').textContent = '';
-    
+    emailError.textContent = '';
+    passwordError.textContent = '';
+
     // Validate email
     if (!form.email) {
-      document.querySelector('.form-error.email').style.color = 'red';
-      document.querySelector('.form-error.email').textContent = 'Email Required.';
+      emailError.style.color = 'red';
+      emailError.textContent = 'Email Required.';
       return;
-    } 
+    }
     if (!testEmail.test(form.email)) {
-      document.querySelector('.form-error.email').style.color = 'red';
-      document.querySelector('.form-error.email').textContent = 'Enter a correct email.';
-      return
+      emailError.style.color = 'red';
+      emailError.textContent = 'Enter a correct email.';
+      return;
     }
 
     // Validate password
     if (!form.password) {
-      document.querySelector('.form-error.password').style.color = 'red';
-      document.querySelector('.form-error.password').textContent = 'Password Required.';
+      passwordError.style.color = 'red';
+      passwordError.textContent = 'Password Required.';
       return;
     }
-    
+
     location.href = 'profile.html';
   })
 
