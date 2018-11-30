0b8734a6c07fe457a6db87309dedbaebc0647e4d
Olivier ESUKA <oesukam@gmail.com>
2018-11-30 09:25:11 +0200

    [Release #162336546] Release

--- server/src/index.js
+++ server/src/index.js
@@ -46,9 +46,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
-app.get('/', (req, res) => {
-  res.send(welcomeMessage);
-});
+app.use('/', express.static('ui/src'));
 
 app.use(express.static('../ui/src'));
 
